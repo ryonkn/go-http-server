a8aae5b80ae6a5e58b9ffe92f74eaab767498e30
Ryo Nakano <ryo@ryonkn.com>
2018-08-08 18:32:32 +0900

    Improve: Add response environments
    Signed-off-by: Ryo Nakano <ryo@ryonkn.com>

--- go-http-server.go
+++ go-http-server.go
@@ -3,13 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<html><body>")
+
+	fmt.Fprintln(w, "<h1>Request Headers<h1>")
 	for key := range r.Header {
-		fmt.Fprintln(w, key, ": ", r.Header[key], "</br>")
+		fmt.Fprintln(w, key, ": ", r.Header[key], "<br>")
 	}
+
+	fmt.Fprintln(w, "<h1>Environments<h1>")
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Fprintln(w, pair[0], ": ", pair[1], "<br>")
+	}
+
 	fmt.Fprintln(w, "</body></html>")
 }
 
