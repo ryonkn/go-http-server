7dfc648f21f0c440415b08d5603a24d4f6fe1f08
Ryo Nakano <ryo@ryonkn.com>
2018-08-09 16:35:41 +0900

    Improve html message
    Signed-off-by: Ryo Nakano <ryo@ryonkn.com>

--- go-http-server.go
+++ go-http-server.go
@@ -9,13 +9,14 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<html><body>")
+	fmt.Fprintln(w, "<h1>", os.Getenv("HOSTNAME"), "</h1>")
 
-	fmt.Fprintln(w, "<h1>Request Headers<h1>")
+	fmt.Fprintln(w, "<h2>Request Headers</h2>")
 	for key := range r.Header {
 		fmt.Fprintln(w, key, ": ", r.Header[key], "<br>")
 	}
 
-	fmt.Fprintln(w, "<h1>Environments<h1>")
+	fmt.Fprintln(w, "<h2>Environments</h2>")
 	for _, e := range os.Environ() {
 		pair := strings.Split(e, "=")
 		fmt.Fprintln(w, pair[0], ": ", pair[1], "<br>")
